d107a4c25abb44507d2142fd729ff54e3281009b
Santiago Quiroz upegui <santiqupgui@gmail.com>
2025-03-07 21:49:49 -0500

    creacion de request

--- SQL/creacion/create.sql
+++ SQL/creacion/create.sql
@@ -20,3 +20,20 @@ CREATE TABLE reference_image (
   reference_id INT NOT NULL,
   FOREIGN KEY (reference_id) REFERENCES reference(id) ON DELETE CASCADE
 );
+
+CREATE TABLE request (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  request_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  request_type VARCHAR(255) NOT NULL, 
+  fullname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) NOT NULL,
+  address VARCHAR(255),
+  product_name VARCHAR(255),
+  reference_id INT,
+  serial_number VARCHAR(255),
+  invoice_id VARCHAR(255),
+  reason TEXT,
+  status VARCHAR(255) DEFAULT 'PENDING',
+  images_base64 LONGTEXT
+);
